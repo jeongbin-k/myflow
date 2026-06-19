f1e9dbafc69531129fb9207ae10d39a3ba2815c2
Jeongbin Kang <kangbin8109@gmail.com>
2026-06-19 15:31:09 +0900

    feat: TodoContext 및 TypeScript 인터페이스 정의

---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+
+// 타입스크립트 투두 데이터 규격(Type)
+export interface Todo {
+  id: number;
+  title: string;
+  is_completed: boolean;
+  category: string;
+  created_at: string;
+  completed_at: string | null;
+}
+// 이 Context 안에서 다른 컴포넌트들에게 "내가 어떤 데이터랑 어떤 함수들을 전역으로 제공할게라고 명시하는 목록 리스트
+export interface TodoContextType {
+  todos: Todo[];
+  isLoading: boolean;
+  fetchTodos: () => Promise<void>;
+  addTodo: (title: string, category: string) => Promise<void>;
+  toggleTodo: (id: number, currentStatus: boolean) => Promise<void>;
+}
+
+export const TodoContext = createContext<TodoContextType | undefined>(
+  undefined,
+);
